cd68e815877028d2c9ff3dab3db4b8e7af43df39
z3r0d4y <jeawon139@gmail.com>
2019-04-15 20:54:31 +0900

    Update KIPS_2019_solidity.sol
    update code

--- KIPS_2019_solidity.sol
+++ KIPS_2019_solidity.sol
@@ -35,6 +35,7 @@ contract VM {
 		uint v_p_id;
 		uint v_c_id;
 		uint v_cvss; // CVE 번호 관리
+		string v_poc; 
 	}
 
 	uint co_no;
@@ -46,7 +47,7 @@ contract VM {
 	
 	function start()
 	{
-		if (status != 1 )
+		if (status != 1)
 		{
 			maker = msg.sender; 
 			status = 1;
@@ -57,37 +58,37 @@ contract VM {
 	}
 
 	// 제품 회사 등록
-	function register_vendor ( string c_name , string c_details , string c_link) returns (uint co_id) 
+	function register_vendor (string c_name , string c_details , string c_link) returns (uint co_id) 
 	{ 
 		co_id = co_no++; 
-		Companys[co_id] = Company ( co_id , c_name , c_details , c_link, msg.sender );
+		Companys[co_id] = Company (co_id , c_name , c_details , c_link, msg.sender);
 	}
 
 	// 제품등록
-	function add_prod(string p_name , string p_details , string p_version , string p_link , uint p_c_id ) returns (uint pr_id) 
+	function add_prod(string p_name , string p_details , string p_version , string p_link , uint p_c_id) returns (uint pr_id) 
 	{
 		if(Companys[p_c_id].c_addr == msg.sender)
 		{
 			pr_id = pr_no++;
-			Products[pr_id]= Product ( pr_id ,p_c_id, p_name , p_details, p_version , p_link ); 	
+			Products[pr_id]= Product(pr_id ,p_c_id, p_name , p_details, p_version , p_link); 	
 		}
 	}
 
 	// 취약점 등록
-	function add_vuln(string v_name , string v_link , uint v_p_id , uint v_c_id ,uint v_cvss ) returns (uint vl_id) 
+	function add_vuln(string v_name , string v_link , uint v_p_id , uint v_c_id, uint v_cvss, v_poc) returns (uint vl_id) 
 	{
 		if(Companys[v_c_id].c_addr == msg.sender)
 		{
 			if (Products[v_p_id].p_id == v_p_id)
 			{
 				vl_id = vl_no++;
-				Vulns[vl_id] = Vuln(vl_id, v_name , v_link, v_p_id, v_c_id,v_cvss);
+				Vulns[vl_id] = Vuln(vl_id, v_name , v_link, v_p_id, v_c_id, v_cvss, v_poc);
 			}	
 		}
 	}
 
 	// 회사 details
-	function list_vendor_details(uint co_id) returns ( uint a, string b , string c , string d ) 
+	function list_vendor_details(uint co_id) returns (uint a, string b , string c , string d) 
 	{
 		if(Companys[co_id].c_id == co_id)
 		{
@@ -112,7 +113,7 @@ contract VM {
 	}
 
 	// 취약점 details
-	function list_vuln_details(uint vl_id) returns (uint a, string b, string c , uint d) 
+	function list_vuln_details(uint vl_id) returns (uint a, string b, string c , uint d, string e) 
 	{
 		if (Vulns[vl_id].vl_id ==vl_id)
 		{
@@ -120,6 +121,7 @@ contract VM {
 			b = Vulns[vl_id].v_name;
 			c = Vulns[vl_id].v_link;
 			d = Vulns[vl_id].v_cvss;
+			e = Vulns[vl_id].v_poc;
 		}
 	}
 }
