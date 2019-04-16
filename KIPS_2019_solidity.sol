615f23656df56cc3310a5e2dc6fd645154342797
z3r0d4y <jeawon139@gmail.com>
2019-04-16 09:50:49 +0900

    Update KIPS_2019_solidity.sol
    update solidity

--- KIPS_2019_solidity.sol
+++ KIPS_2019_solidity.sol
@@ -34,7 +34,7 @@ contract VM {
 		string v_link;
 		uint v_p_id;
 		uint v_c_id;
-		uint v_cvss; // CVE 번호 관리
+		uint v_cve_num; // CVE 번호 관리
 		string v_poc; 
 	}
 
@@ -58,37 +58,37 @@ contract VM {
 	}
 
 	// 제품 회사 등록
-	function register_vendor (string c_name , string c_details , string c_link) returns (uint co_id) 
+	function register_vendor (string c_name, string c_details, string c_link) returns (uint co_id) 
 	{ 
 		co_id = co_no++; 
-		Companys[co_id] = Company (co_id , c_name , c_details , c_link, msg.sender);
+		Companys[co_id] = Company (co_id, c_name, c_details, c_link, msg.sender);
 	}
 
 	// 제품등록
-	function add_prod(string p_name , string p_details , string p_version , string p_link , uint p_c_id) returns (uint pr_id) 
+	function add_prod(string p_name, string p_details, string p_version, string p_link, uint p_c_id) returns (uint pr_id) 
 	{
 		if(Companys[p_c_id].c_addr == msg.sender)
 		{
 			pr_id = pr_no++;
-			Products[pr_id]= Product(pr_id ,p_c_id, p_name , p_details, p_version , p_link); 	
+			Products[pr_id]= Product(pr_id, p_c_id, p_name, p_details, p_version, p_link); 	
 		}
 	}
 
 	// 취약점 등록
-	function add_vuln(string v_name , string v_link , uint v_p_id , uint v_c_id, uint v_cvss, v_poc) returns (uint vl_id) 
+	function add_vuln(string v_name, string v_link, uint v_p_id, uint v_c_id, uint v_cve_num, v_poc) returns (uint vl_id) 
 	{
 		if(Companys[v_c_id].c_addr == msg.sender)
 		{
 			if (Products[v_p_id].p_id == v_p_id)
 			{
 				vl_id = vl_no++;
-				Vulns[vl_id] = Vuln(vl_id, v_name , v_link, v_p_id, v_c_id, v_cvss, v_poc);
+				Vulns[vl_id] = Vuln(vl_id, v_name, v_link, v_p_id, v_c_id, v_cve_num, v_poc);
 			}	
 		}
 	}
 
 	// 회사 details
-	function list_vendor_details(uint co_id) returns (uint a, string b , string c , string d) 
+	function list_vendor_details(uint co_id) returns (uint a, string b, string c, string d) 
 	{
 		if(Companys[co_id].c_id == co_id)
 		{
@@ -100,7 +100,7 @@ contract VM {
 	}
 
 	// 제품 details
-	function list_products_details(uint p_id) returns (uint a, string b, string c , string d, string e) 
+	function list_products_details(uint p_id) returns (uint a, string b, string c, string d, string e) 
 	{
 		if(Products[p_id].p_id ==p_id)
 		{
@@ -113,14 +113,14 @@ contract VM {
 	}
 
 	// 취약점 details
-	function list_vuln_details(uint vl_id) returns (uint a, string b, string c , uint d, string e) 
+	function list_vuln_details(uint vl_id) returns (uint a, string b, string c, uint d, string e) 
 	{
 		if (Vulns[vl_id].vl_id ==vl_id)
 		{
 			a = Vulns[vl_id].vl_id;
 			b = Vulns[vl_id].v_name;
 			c = Vulns[vl_id].v_link;
-			d = Vulns[vl_id].v_cvss;
+			d = Vulns[vl_id].v_cve_num;
 			e = Vulns[vl_id].v_poc;
 		}
 	}
